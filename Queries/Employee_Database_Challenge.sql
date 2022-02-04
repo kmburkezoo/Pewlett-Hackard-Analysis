a7404a3dc58194fe718d1c29bbc67f8991dadfa0
Katelin Burke <katelinburke@gmail.com>
2022-02-04 16:17:48 -0700

    readme and supporting images

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -31,7 +31,7 @@ order by
 	emp_no,
 	to_date desc
 ;
-
+select count(emp_no) from unique_titles;
 
 select
 	title,
@@ -39,6 +39,27 @@ select
 into retiring_titles
 from unique_titles
 group by title
+order by 2 desc
+;
+select * from retiring_titles;
+
+/*
+by department instead of title
+*/
+
+select
+	d.dept_name,
+-- 	ut.title,
+	count(ut.emp_no)
+from unique_titles ut
+	join dept_emp de
+		on ut.emp_no = de.emp_no
+		and de.to_date = '9999-01-01'
+	join departments d
+		on de.dept_no = d.dept_no
+group by 
+	d.dept_name/*,
+	ut.title*/
 order by 2 desc
 ;
 
@@ -66,4 +87,61 @@ order by
 	e.emp_no,
 	t.to_date desc
 ;
+select count(*) from mentorship_eligibility;
 
+--distribution of eligible mentors by department and role
+select 
+	d.dept_name,
+-- 	me.title,
+	count(distinct me.emp_no)
+from mentorship_eligibility me
+	join dept_emp de
+		on me.emp_no = de.emp_no
+		and de.to_date = '9999-01-01'
+	join departments d
+		on de.dept_no = d.dept_no
+-- where me.title like 'Senior%' or me.title like '%Lead%'-- or me.title like '%Manager%'
+group by d.dept_name--, me.title
+order by 1--,3 desc
+;
+
+--eligible mentors vs non-retiring employees by department
+create view mentorship_by_dept_vw as
+(
+select 
+	d.dept_name,
+	count(ut.emp_no) non_mentor_retirees,
+	count(me.emp_no) mentors,
+	count(e.emp_no) mentees
+from dept_emp de
+	left join mentorship_eligibility me
+		on de.emp_no = me.emp_no
+	left join unique_titles ut
+		on de.emp_no = ut.emp_no
+		and ut.emp_no not in (select emp_no from mentorship_eligibility)
+	left join employees e
+		on de.emp_no = e.emp_no
+		and e.birth_date > '1955-12-31'
+	join departments d
+		on de.dept_no = d.dept_no
+where de.to_date = '9999-01-01'
+group by dept_name
+order by 1
+)
+;
+/*
+are there really only january birthdays in this pool?
+yes. queries are correct but data is suspect
+*/
+select distinct birth_date 
+from employees e
+	join dept_emp de
+		on e.emp_no = de.emp_no
+where (e.birth_date between '1965-01-01' and '1965-12-31')
+	and de.to_date = '9999-01-01'
+;
+
+select count(emp_no) from unique_titles; --72458;
+select count(emp_no) from dept_emp where to_date = '9999-01-01';--240124
+
+72458/240124
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Pewlett-Hackard-Analysis
+
+## Overview
+This project involves loading CSV-formatted spreadsheets into a database, then using SQL queries to analyze larger quantities of data than are easily consumable in spreadsheet form. The analysis centers around determining which employees from this very large dataset are likely to retire soon and what departments they are retiring from in order to prepare management for the large-scale anticipated turnover.
+
+![Employee Database ERD](employee_db.png)
+
+## Results
+
+### Retiring Employees by Title (and Department)
+
+|Retiring Employees by Title|Retiring Employees by Department|
+|---|---|
+|![retiree counts by title](retirees_by_title.png)|![retiree counts by department](retirees_by_dept.png) 
+
+* The majority of expected retirees hold senior positions. It will be harder to find suitable replacements for these positions, so Pewlett-Hackard would do well to start looking early, or to focus on employee deployment so that existing employees can be promoted into these positions when they become vacant.
+* The Development and Production Departments will be hit hardest by surging retirement, so internal development efforts will likely be most fruitful here.
+* Given the number of expected retirees, shockingly few are managers. Pewlett-Hackard might want to look into its promotion practices or the accuracy of its data.
+
+### Mentorship Eligibility
+* While the number of retiring employees is enormous, very few fit the given criteria for mentorship. Pewlett-Hackard may want to consider broadening the pool of candidates eligible for mentorship beyond those born in 1965 in order to ensure that enough younger employees can be mentored before the retirement wave hits.
+![mentorship_by_dept_vw](mentorship_by_dept.png)
+
+## Summary
+* 72,458 employees are expected to retire in the next few years, over 30% of the company.
+* Only 1,549 expected retirees meet the given criteria for mentorship, nowhere near enough to provide enough guidance and support for the development of the younger generation of employees.
